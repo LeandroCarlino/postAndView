abf903d1c1bd5d83fa45cceaa24264ec4f5a2491
Leandro Carlino <leandro.ca.98@gmail.com>
2022-06-06 13:55:52 -0300

    create components and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CommentsComponent } from './components/comments/comments.component';
+import { FormComponent } from './components/form/form.component';
+import { PostDetailComponent } from './components/post-detail/post-detail.component';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { HomeComponent } from './components/shared/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'post-list', component: PostListComponent},
+  {path:'post-detail/:id', component: PostDetailComponent},
+  {path:'comments', component: CommentsComponent},
+  {path:'form', component: FormComponent},
+  {path:'**', pathMatch: 'full', redirectTo: 'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/shared/home/home.component';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { PostDetailComponent } from './components/post-detail/post-detail.component';
+import { CommentsComponent } from './components/comments/comments.component';
+import { FormComponent } from './components/form/form.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PostListComponent,
+    PostDetailComponent,
+    CommentsComponent,
+    FormComponent,
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
